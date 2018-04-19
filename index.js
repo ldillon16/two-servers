06ac9031901a0e475e5cef54c64abcfcab99a918
lndillon <lndillon>
2018-04-19 19:26:20 -0400

    added insults

--- index.js
+++ index.js
@@ -15,10 +15,16 @@ var server2 = http.createServer(handleRequest2)
 
 server1.listen(7000, function() {
 	console.log("server listening on: http://localhost:7000");
-	console.log("you're not ugly");
+	var x = Math.floor(Math.random() * 4);
+	console.log(arr1[x]);
 });
 
 server2.listen(7500, function() {
 	console.log("server listening on: http://localhost:7500");
-	console.log("you're ugly");
+	var x = Math.floor(Math.random() * 5);
+	console.log(arr2[x]);
 });
+
+var arr1 = ["you're beautiful", "you're smart", "you're cool", "you're driving down the highway, late as can be but you're making it happen!"];
+
+var arr2 = ["you're ugly", "you're stupid", "you're lame", "you're a big dumbass", "you look like crap"];
